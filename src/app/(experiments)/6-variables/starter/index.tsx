88451efb7631fcedbf660e82b19ac1289a44d248
Vitor Franco <vitoroliveirafranco@gmail.com>
2026-02-17 13:36:28 -0300

    refactor: move screen dimensions logging inside useEffect for clarity

--- src/app/(experiments)/6-variables/starter/index.tsx
+++ src/app/(experiments)/6-variables/starter/index.tsx
@@ -11,19 +11,38 @@ export default function Page() {
   useEffect(() => {
     const controller = new AbortController();
 
+    const screenW = window.innerWidth;
+    const screenH = window.innerHeight;
+    console.log({
+      screenW,
+      screenH,
+    })
+
     window.addEventListener("mousemove", (event) => {
       const mouseX = event.clientX;
       const mouseY = event.clientY;
-
-      const screenW = window.innerWidth;
-      const screenH = window.innerHeight;
+      console.log("")
+      console.log("")
+      console.log("")
+      console.log({
+        mouseX,
+        mouseY,
+      })
 
       const centerX = screenW / 2;
       const centerY = screenH / 2;
+      console.log({
+        centerX,
+        centerY,
+      })
 
       const d = distance(mouseX, mouseY, centerX, centerY);
-      const maxDistance = distance(0, 0, centerX, centerY);
-      
+      const maxDistance = distance(0, 0, centerX, centerY); // the max distance is from corner to center.
+
+      console.log("d", d)
+      console.log("maxDistance", maxDistance)
+      console.log("final", d / maxDistance)
+
       if (titleRef.current) {
         titleRef.current.style.setProperty("--distance", `${d / maxDistance}`);
       }
