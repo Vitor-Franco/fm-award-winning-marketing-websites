d4f213bff8886ad1a99a21df927e19dc7910e57a
Vitor Franco <vitoroliveirafranco@gmail.com>
2026-02-17 11:04:50 -0300

    add css version of gsap animation first example

--- src/app/(experiments)/2-css-version/starter/index.tsx
+++ src/app/(experiments)/2-css-version/starter/index.tsx
@@ -1,11 +1,20 @@
+import s from "./style.module.css"
+
+const text = ["CSS".split(""), "version".split("")];
+
 export default function Page() {
   return (
-    <div className="bg-blue-300 text-black">
-      <div className="flex h-screen items-end justify-left overflow-hidden">
+    <div className="text-black bg-blue-300">
+      <div className="flex items-end h-screen overflow-hidden justify-left">
         <h1 className="title font-black text-[min(20rem,30vw)] leading-none pb-[0.1em] text-left">
-          CSS
+          {text[0].map((letter, i) => <span className={s.letter}
+            style={{
+              "--index": i,
+            } as React.CSSProperties} key={i}>{letter}</span>)}
           <br />
-          version
+          {text[1].map((letter, i) => <span className={s.letter} style={{
+            "--index": i,
+          } as React.CSSProperties} key={i}>{letter}</span>)}
         </h1>
       </div>
     </div>
