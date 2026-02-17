dd0b2543d661bee96234c49d4219e3da4abdb859
Vitor Franco <vitoroliveirafranco@gmail.com>
2026-02-17 12:30:43 -0300

    add leva to add some debugtool for our designers

--- src/app/(experiments)/4-scroll-trigger/starter/title.tsx
+++ src/app/(experiments)/4-scroll-trigger/starter/title.tsx
@@ -4,11 +4,22 @@ import { useGSAP } from "@gsap/react";
 import { useRef } from "react";
 import { SplitText } from "gsap/all";
 import gsap from "gsap";
+import { useControls } from "leva";
 
 gsap.registerPlugin(SplitText);
 
 export function TitleSection() {
   const containerRef = useRef<HTMLDivElement>(null);
+
+  const { stagger } = useControls({
+    stagger: {
+      value: 0.1,
+      min: 0,
+      max: 1,
+      step: 0.1
+    }
+  })
+
   useGSAP(
     () => {
       SplitText.create("h1", {
@@ -19,25 +30,29 @@ export function TitleSection() {
 
       gsap.from("h1 .word", {
         y: "100%",
-        stagger: 0.1,
+        stagger: stagger,
         ease: "circ.inOut",
       });
     },
-    { scope: containerRef }
+    { 
+      scope: containerRef,
+      dependencies: [stagger],
+      revertOnUpdate: true, // if dependencies changes, revert animations
+    }
   );
   return (
     <div
       ref={containerRef}
-      className="flex h-screen items-center justify-center"
+      className="flex items-center justify-center h-screen"
     >
       <h1 className="title font-black text-[10vh] leading-[1.3] pb-[0.1em] flex flex-col gap-[0.1em] relative right-[0.5em] [&>span]:-mb-[0.3em]">
         <span className="block relative text-left text-(--dark-green) italic font-thin">
           gsap
         </span>
-        <span className="block relative text-right uppercase font-thin">
+        <span className="relative block font-thin text-right uppercase">
           scroll
         </span>
-        <span className="block relative text-left uppercase font-thin">
+        <span className="relative block font-thin text-left uppercase">
           triggered
         </span>
         <span className="block relative left-[1.5em] text-right text-(--dark-green) italic font-thin">
